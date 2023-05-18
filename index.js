0a25364f50cb87714d724526bb935d7927711b1d
abdulahaddf <abdulahad.df1@gmail.com>
2023-05-18 18:02:27 +0600

    delete toy

--- index.js
+++ index.js
@@ -72,7 +72,12 @@ async function run() {
             res.send(result);
         })
 
-        
+        app.delete('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
