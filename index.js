4400db875fd69b4d62a80f7f130f32d117a69190
abdulahaddf <abdulahad.df1@gmail.com>
2023-05-18 18:00:53 +0600

    post toy

--- index.js
+++ index.js
@@ -9,8 +9,50 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb+srv://abdulahaddf1:BpwyQcr841PRWoPQ@cluster0.wg0iu7v.mongodb.net/?retryWrites=true&w=majority";
 
 
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const toyCollection = client.db('toyDB').collection('toy');
+
+        
+
+        app.post('/toy', async (req, res) => {
+            const newtoy = req.body;
+            console.log(newtoy);
+            const result = await toyCollection.insertOne(newtoy);
+            res.send(result);
+        })
+
+      
+
+        
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
